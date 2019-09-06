1144a89f53580ee52662a0005543179f6b6840b3
Marianne Cordova <macoam00@hotmail.com>
2019-09-06 08:46:37 -0700

    Agregar archivos de proyecto.

---
+++ Herencia/Figura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    abstract class Figura
+    {
+        public string Color { get; set; }
+        
+        //Todas la figuras deben ser capaces de calcular su área
+        public abstract double calcularArea();
+
+        public string toString()
+        {
+            return " \n Soy una figura: " + Color;
+        }
+    }
+}
---
+++ Herencia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo();
+
+            triangulo.Color = "Morado";
+
+            Console.WriteLine(triangulo.toString());
+
+            Console.Read();
+        }
+    }
+}
